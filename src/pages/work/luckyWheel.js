0403c6fad80d99203e9d4450dbec6b4452934615
chouys00 <chouys00@gmail.com>
2021-07-27 21:51:48 +0800

    react webpack.config

--- src/pages/work/luckyWheel.js
+++ src/pages/work/luckyWheel.js
@@ -1,16 +1,32 @@
 
 import './luckyWheel.scss'
-import React, {useEffect} from 'react';
+import React, {useState,useEffect } from 'react';
 import { renderRoutes } from 'react-router-config';
+import wheel from '../../assets/luckyWheel/obj_02.png'
+import start from '../../assets/luckyWheel/obj_01.png'
 
+const Work = ({route}) => {
+    const [a,seta]=useState(0)
 
-const work = ({route}) => {
+    let wheelStyle={
+        // transform:'rotate('+deg+'deg)',transition:a
+        // width: "10px",
+        transform: "rotate(900deg)"
+    }
+
+    const startFunc = ()=>{
+        console.log(111111111111,)
+    }
+
     return (
-        <>
-            <img src="@/assets/luckyWheel/obj_01.png"></img>
+        <div className='luckyWheelWrap'>
+            <div>
+                <img className='wheelPic' src={wheel} style={wheelStyle}></img>
+                <img className='startPic' src={start} onClick={startFunc}></img>
+            </div>
             {/* {renderRoutes(route.routes)} */}
-        </>    
+        </div>
     )
 }
 
-export default work;
+export default Work;
